690f13d1b6105a6ce568db6c2f5861991e2e40b5
Shrutika <shrutikajadhav2611@gmail.com>
2024-04-21 02:05:32 +0530

    Initial commit

--- menterbot/src/App.js
+++ menterbot/src/App.js
@@ -1,9 +1,14 @@
+
 import React, { useState } from 'react';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import './App.css';
 import Header from './header/Header';
 import Navbar from './Navbar/Navbar';
 import Banner from './Banner/Banner';
 import Blogs from './Blog/Blogs';
+import Testimonials from "./Components/Testimonials";
+
 
 function App() {
   const [backgroundImage, setBackgroundImage] = useState('');
@@ -12,7 +17,7 @@ function App() {
     <div className="App">
       <Header/>
       <Navbar/>
-      <Banner/>
+      <Banner/> 
       <div className="content-container">
         <div className="mentorbot-container">
           <h2>MENTORBOT</h2>
@@ -22,8 +27,9 @@ function App() {
             <p>We understand that selecting on a career path can be a difficult and daunting experience, which is why we provide complete career counselling services in order to assist individuals in identifying their talents and interests and navigating the employment market. Our skilled consultants work with each person to develop an individually tailored strategy that is in line with their goals and aspirations.</p>
           </div>
         </div>
-      </div>
-      <Blogs/>
+      </div> 
+       <Blogs/>
+      <Testimonials/>
     </div>
   );
 }
