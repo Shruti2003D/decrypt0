17f0e5fe917a2ab9fb4945bde8fe9b90a3912c19
iommie2002 <awariomkar3@gmail.com>
2024-04-24 14:05:24 +0530

    navbar made transparent

--- menterbot/src/Navbar/Navbar.js
+++ menterbot/src/Navbar/Navbar.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import './Navbar.css'; // Import the CSS file
-import logo from '../assets/menterbotlogo.png'; // Assuming this is the path to your logo image
+import './Navbar.css'; // Import CSS file for styling
+import logo from '../assets/menterbotlogo.png';
 
-const Navbar = ({ backgroundColor }) => {
+const Navbar = () => {
   return (
-    <nav className="Navbar" style={{ backgroundColor }}>
+    <nav className="Navbar">
       <div className="logo">
-        <img src={logo} alt="Menterbot Logo" />
+        <img src={logo} alt="Menterbot Logo" className="logo-img" />
       </div>
       <div className="navbar-links">
-        <a href="#blogs">Blogs</a>
-        <a href="#about">About</a>
-        <a href="#contact">Contact</a>
-        <a href="#careers">Careers</a>
-        <a href="#services">Services</a>
-        <a href="#courses">Courses</a>
+        <a href="/blogs" className="navbar-text hover-effect">Blogs</a>
+        <a href="/about" className="navbar-text hover-effect">About</a>
+        <a href="/contact" className="navbar-text hover-effect">Contact</a>
+        <a href="/careers" className="navbar-text hover-effect">Careers</a>
+        <div className="dropdown">
+          <a href="/courses" className="navbar-text hover-effect">Courses</a>
+          <div className="dropdown-content">
+            <a href="/courses/beginner" className="navbar-text">Beginner Level</a>
+            <a href="/courses/intermediate" className="navbar-text">Intermediate Level</a>
+            <a href="/courses/advanced" className="navbar-text">Advanced Level</a>
+          </div>
+        </div>
       </div>
       <div className="login-signup">
-        <a href="#login" className="login-button">Login</a>
-        <a href="#signup" className="signup-button">Sign Up</a>
+        <a href="/login" className="login-button hover-effect">Login</a>
+        <a href="/signup" className="signup-button hover-effect">Sign Up</a>
       </div>
     </nav>
   );
