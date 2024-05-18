e117a98941a9eef7e7732ba772a8000487b2b3c1
Om Jangde <om.jangade@mitaoe.ac.in>
2024-05-19 01:24:18 +0530

    Login and Sign-up Page is completed - 19/05/2024

--- menterbot/src/Components/login.jsx
+++ menterbot/src/Components/login.jsx
@@ -1,14 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import '../css/login.css';
+import Footer from "./Footer";
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [rememberMe, setRememberMe] = useState(false);
   return (
-    <div>
-      <h2>Login</h2>
-      <h2>Login</h2>
-      <h2>Login</h2>
-      <h2>Login</h2>
-      {/* Add your login form here */}
-    </div>
+    <>
+    <section id='login'>
+      <div className='container'>
+        <div className='login-box'>
+          <form className='login-form'>
+            <h1>✨Login✨</h1>
+            <div className='input-group'>
+              <label>Email </label>
+              <div className='input-wrapper'>
+                <input
+                  type='email'
+                  placeholder=''
+                  name='email'
+                  className='input-field' />
+              </div>
+            </div>
+            <div className='input-group'>
+              <label>Password </label>
+              <div className='input-wrapper'>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder=''
+                  name='password'
+                  className='input-field' />
+                <div className='toggle-password' onClick={() => setShowPassword(prev => !prev)}>
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </div>
+              </div>
+              <div className='input-group remember-me'>
+                <label htmlFor='rememberMe'>Remember me</label>
+                <input
+                  type='checkbox'
+                  id='rememberMe'
+                  checked={rememberMe}
+                  onChange={() => setRememberMe(prev => !prev)}
+                />
+              </div>
+              <p className='signup-link'>
+                Don't have an account? <Link to="/signup" className='signup-link-highlight'>Register</Link>
+                <button className='login'>Log in</button>
+              </p>
+              
+
+              <Link to='/forgot-password' className='forgot-password-link'>
+                Forgot your password ?
+              </Link>
+            </div>
+          </form>
+
+        </div>
+      </div>
+    </section>
+    <br/>
+    <br/>
+    <Footer />
+    </>
   );
 };
 
--- menterbot/src/Components/signup.jsx
+++ menterbot/src/Components/signup.jsx
@@ -1,11 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import '../css/signup.css';
+import Footer from "./Footer";
 
 const Signup = () => {
+  const [rememberMe, setRememberMe] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  
   return (
-    <div>
-      <h2>Sign Up</h2>
-      {/* Add your sign up form here */}
-    </div>
+    <>
+    <section id='login'>
+      <div className='container'>
+        <div className='login-box'>
+          <form className='login-form'>
+            <h1>✨Create Student Free Account✨</h1>
+            <div className='input-group'>
+              <label>Name </label>
+              <div className='input-wrapper'>
+                <input
+                  type='text'
+                  placeholder=''
+                  name='name'
+                  className='input-field' />
+              </div>
+            </div>
+            <div className='input-group'>
+              <label>Email </label>
+              <div className='input-wrapper'>
+                <input
+                  type='email'
+                  placeholder=''
+                  name='email'
+                  className='input-field' />
+              </div>
+            </div>
+            <div className='input-group'>
+              <label>Phone Number </label>
+              <div className='input-wrapper'>
+                <input
+                  type='text'
+                  placeholder=''
+                  name='phone'
+                  className='input-field' />
+              </div>
+            </div>
+            <div className='input-group'>
+              <label>Password </label>
+              <div className='input-wrapper'>
+                <input
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder=''
+                  name='password'
+                  className='input-field' />
+                <div className='toggle-password' onClick={() => setShowPassword(prev => !prev)}>
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </div>
+              </div>
+            </div>
+            <div className='input-group'>
+              <label>Confirm Password </label>
+              <div className='input-wrapper'>
+                <input
+                  type={showConfirmPassword ? 'text' : 'password'}
+                  placeholder=''
+                  name='confirmPassword'
+                  className='input-field' />
+                <div className='toggle-password' onClick={() => setShowConfirmPassword(prev => !prev)}>
+                  {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+                </div>
+              </div>
+            </div>
+            <div className='input-group remember-me'>
+              <label htmlFor='rememberMe'>I agree to the Terms and Conditions</label>
+              <input
+                type='checkbox'
+                id='rememberMe'
+                checked={rememberMe}
+                onChange={() => setRememberMe(prev => !prev)}
+              />
+            </div>
+            <p className='signup-link'>
+              Already have an account? <Link to="/login" className='signup-link-highlight'>Login</Link>
+              <button className='login'>Register</button>
+            </p>
+          </form>
+        </div>
+      </div>
+    </section>
+    <Footer />
+    </>
   );
 };
 
