342b0b7045d34e056e4c8a3699927ba641337691
Shruti2003D <shruti.dhumal@mitaoe.ac.in>
2024-05-21 16:58:50 +0530

    about us done

--- menterbot/src/Components/About.jsx
+++ menterbot/src/Components/About.jsx
@@ -4,13 +4,12 @@ import abhishekPhoto from '../assets/Abhishek.jpg';
 import hrishabhPhoto from '../assets/Hrishab.jpg';
 import khushiPhoto from '../assets/Khushi.jpeg';
 import '../css/About.css';
-
+import Footer from "./Footer";
 const About = () => {
   // Accessing route parameters using useParams hook
   let { id } = useParams();
 
   return (
-    
     <section className="about-us-section">
       <h1 className="about-us-title">About Us</h1>
       <div className="motivation-box">
@@ -25,7 +24,6 @@ const About = () => {
         </div>
       </div>
 
-      
       <h2 className="about-us-subtitle">Meet the amazing people behind our success</h2>
       <div className="team-container">
         <div className="team-member">
@@ -34,18 +32,10 @@ const About = () => {
             <h3 className="member-name">Abhishek Chouriya</h3>
             <p className="member-role">Founder</p>
             <p className="member-description">
-            Abhishek Chouriya is a cyber security trainer, public speaker, and proponent of 
-            student development. With years of experience in the field, he is passionate about 
-            teaching others about the importance of cyber security and the steps that individuals 
-            and companies can take to protect themselves </p>
+              Abhishek Chouriya is a cyber security trainer, public speaker, and proponent of student development. With years of experience in the field, he is passionate about teaching others about the importance of cyber security and the steps that individuals and companies can take to protect themselves.
+            </p>
             <p className="member-description">
-            Abhishek is dedicated to supporting students in accomplishing their goals and reaching their
-             full potential, in addition to his employment in cyber security. He has developed a number of
-             training programmes and courses for people and groups of all skill levels. Through his public speaking engagements
-             , he offers his skills and encourages audiences to take action to protect themselves and their
-              organisations from cyber threats. Abhishek is making a significant impact in his community and
-               beyond
-
+              Abhishek is dedicated to supporting students in accomplishing their goals and reaching their full potential. He has developed a number of training programmes and courses for people and groups of all skill levels. Through his public speaking engagements, he offers his skills and encourages audiences to take action to protect themselves and their organisations from cyber threats. Abhishek is making a significant impact in his community and beyond.
             </p>
           </div>
         </div>
@@ -55,7 +45,10 @@ const About = () => {
             <h3 className="member-name">Hrishabh Chhipa</h3>
             <p className="member-role">Co-Founder</p>
             <p className="member-description">
-              Hrishabh Chhipa, one of the co-founders of MentorBot, is a strategic problem-solver and economics graduate. With a focus on team orientation and deep thinking, he has been instrumental in driving MentorBot's success.
+              Meet Hrishabh Chhipa, one of the co-founders of MENTORBOT. He has a special set of abilities and is a strategic problem-solver in addition to being an economics graduate. With a focus on team orientation and deep thinking, he has been instrumental in driving Mentorbot's success.
+            </p>
+            <p className="member-description">
+              As a content editor and reviewer, he ensures the highest standards of quality for all written materials produced by the company. Additionally, he brings a fresh viewpoint to the team as a dynamic public speaker who enjoys talking about mindset, personal development, and AI.
             </p>
           </div>
         </div>
@@ -65,14 +58,18 @@ const About = () => {
             <h3 className="member-name">Khushi Jain</h3>
             <p className="member-role">Associate</p>
             <p className="member-description">
-              Khushi Jain, a final-year BPT student, also works as a physiotherapist. She brings a unique perspective to the team with an abundance of expertise in the sector and outstanding communication skills.
+              Meet Khushi Jain, a committed MentorBot team member. She is a final-year BPT, or Bachelor of Physiotherapy, student who also works as a physiotherapist. She adds a unique viewpoint to the team with an abundance of expertise in the sector and outstanding communication abilities.
+            </p>
+            <p className="member-description">
+              Khushi is an important member of the MentorBot team, offering assistance and direction to clients with their physiotherapy preparation. Her passion for assisting others, combined with her great communication abilities, makes her a vital member of the MentorBot team.
             </p>
           </div>
         </div>
       </div>
+      <Footer></Footer>
     </section>
+
   );
 };
-
 
 export default About;
