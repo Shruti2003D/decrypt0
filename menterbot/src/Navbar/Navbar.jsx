5bd5b1c3dc86afdd1403bb12d03c96246f930fde
Om Jangde <om.jangade@mitaoe.ac.in>
2024-05-27 00:53:27 +0530

    Login & Sign-Up Page - 27/05/24

--- menterbot/src/Navbar/Navbar.jsx
+++ menterbot/src/Navbar/Navbar.jsx
@@ -1,66 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import './Navbar.css';
-import logo from '../assets/menterbotlogo.png';
-import { Link } from 'react-router-dom';
-import Header from '../header/Header';
+  import React, { useState, useEffect } from 'react';
+  import './Navbar.css';
+  import logo from '../assets/menterbotlogo.png';
+  import { Link } from 'react-router-dom';
+  import Header from '../header/Header';
+  import Login from '../Components/login';
+  import Signup from '../Components/signup';
 
-const Navbar = () => {
-  const [hideNavbar, setHideNavbar] = useState(false);
-  const [lastScrollTop, setLastScrollTop] = useState(0);
+  const Navbar = () => {
+    const [openLoginPage, setOpenLoginPage] = useState(false);
+    const [openSignUpPage, setOpenSignUpPage] = useState(false);
+    const [hideNavbar, setHideNavbar] = useState(false);
+    const [lastScrollTop, setLastScrollTop] = useState(0);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop = window.scrollY;
+    useEffect(() => {
+      const handleScroll = () => {
+        const scrollTop = window.scrollY;
 
-      if (scrollTop > lastScrollTop) {
-        // Scrolling down
-        setHideNavbar(true);
-      } else {
-        // Scrolling up
-        setHideNavbar(false);
-      }
-      setLastScrollTop(scrollTop);
-    };
+        if (scrollTop > lastScrollTop) {
+          // Scrolling down
+          setHideNavbar(true);
+        } else {
+          // Scrolling up
+          setHideNavbar(false);
+        }
+        setLastScrollTop(scrollTop);
+      };
 
-    window.addEventListener('scroll', handleScroll);
+      window.addEventListener('scroll', handleScroll);
 
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [lastScrollTop]);
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, [lastScrollTop]);
 
-  return (
-    <>
-      <Header />
-      <nav className={`Navbar ${hideNavbar ? 'hide' : ''}`}>
-        <div className="navbar-container">
-          <div className="logo">
-            <Link to="/">
-              <img src={logo} alt="Menterbot Logo" className="logo-img" />
-            </Link>
-          </div>
-          <div className="navbar-links">
-            <Link to="/blogs" className="navbar-text hover-effect">Blogs</Link>
-            <Link to="/about" className="navbar-text hover-effect">About</Link>
-            <Link to="/contact" className="navbar-text hover-effect">Contact</Link>
-            <Link to="/careers" className="navbar-text hover-effect">Careers</Link>
-            <div className="dropdown">
-              <Link to="/courses" className="navbar-text hover-effect">Courses</Link>
-              <div className="dropdown-content">
-                <Link to="/courses/beginner" className="navbar-text">Beginner Level</Link>
-                <Link to="/courses/intermediate" className="navbar-text">Intermediate Level</Link>
-                <Link to="/courses/advanced" className="navbar-text">Advanced Level</Link>
+    return (
+      <>
+        <Header />
+        <nav className={`Navbar ${hideNavbar ? 'hide' : ''}`}>
+          <div className="navbar-container">
+            <div className="logo">
+              <Link to="/">
+                <img src={logo} alt="Menterbot Logo" className="logo-img" />
+              </Link>
+            </div>
+            <div className="navbar-links">
+              <Link to="/blogs" className="navbar-text hover-effect">Blogs</Link>
+              <Link to="/about" className="navbar-text hover-effect">About</Link>
+              <Link to="/contact" className="navbar-text hover-effect">Contact</Link>
+              <Link to="/careers" className="navbar-text hover-effect">Careers</Link>
+              <div className="dropdown">
+                <Link to="/courses" className="navbar-text hover-effect">Courses</Link>
+                <div className="dropdown-content">
+                  <Link to="/courses/beginner" className="navbar-text">Beginner Level</Link>
+                  <Link to="/courses/intermediate" className="navbar-text">Intermediate Level</Link>
+                  <Link to="/courses/advanced" className="navbar-text">Advanced Level</Link>
+                </div>
               </div>
             </div>
+            <div className="login-signup">
+              <button className='login-button hover-effect' onClick={() => setOpenLoginPage(true)}>Login</button>
+              <button className='signup-button hover-effect' onClick={() => setOpenSignUpPage(true)}>Sign Up</button>
+            </div>
           </div>
-          <div className="login-signup">
-            <Link to="/login" className="login-button hover-effect">Login</Link>
-            <Link to="/signup" className="signup-button hover-effect">Sign Up</Link>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
-};
+          {openLoginPage && (
+            <Login onClose={() => setOpenLoginPage(false)}/>
+          )}
+          {openSignUpPage && (
+            <Signup onClose={() => setOpenSignUpPage(false)}/>
+          )}
+        </nav>
+      </>
+    );
+  };
 
-export default Navbar;
+  export default Navbar;
