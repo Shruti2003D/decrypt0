f498d3a9d3dcc21ab44e9241d9ed72152cfc83f1
Shrutika <shrutikajadhav2611@gmail.com>
2024-05-28 16:21:12 +0530

    contact

--- menterbot/src/App.js
+++ menterbot/src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import Navbar from './Navbar/Navbar';
 import About from './Components/About';
-import Contact from './Components/Contact';
+import Contact from './Components/ContactUs';
 import Careers from './Components/Career';
 import Courses from './Components/Courses';
 import Login from './Components/login';
---
+++ menterbot/src/Components/ContactUs.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "../css/ContactUs.css"; // Make sure to import the CSS file
+
+const ContactUs = () => {
+  return (
+    <div className="top_heading_out">
+      <div className="top_site_main" style={{ color: 'rgb(255, 255, 255)', backgroundImage: 'url("https://eduma.thimpress.com/demo-main/wp-content/uploads/sites/95/2022/11/Eduma-cover-banner-17.jpg")', paddingTop: '400px' }}>
+        <div className="overlay-top-header" style={{ backgroundColor: 'rgba(0,0,0,0.5)' }}></div>
+        <div className="page-title-wrapper">
+          <div className="banner-wrapper container">
+            <h1 className="page-title">Contact Us</h1>
+          </div>
+        </div>
+      </div>
+      <div className="breadcrumbs-wrapper">
+        <div className="container">
+          <ul className="breadcrumbs" id="breadcrumbs">
+            <li><a href="C:\Users\INDRAJIT\OneDrive\Desktop\Decrypt0\decrypt0\menterbot\src\Components\Home.jsx">Home  |</a></li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    
+  );
+};
+
+export default ContactUs;
