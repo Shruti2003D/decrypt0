b6fe7d042640781563353fe47d884bbd9109ee3b
testproject <mohakhamb76@gmail.com>
2024-06-06 09:01:26 +0530

    Home Blog CSS Issue Resolve

--- menterbot/src/Components/Blogs.js
+++ menterbot/src/Components/Blogs.js
@@ -7,20 +7,20 @@ const Blogs = () => {
 
       <h2>Read From Our Latest Blogs.</h2>
 
-      <div className='cards'>
-        <div className="card">
+      <div className='cards-blogs'>
+        <div className="card-blog">
           <span className="date">18 April 2024</span>
           <h3>Unlocking Security Insights: How SIEM Tools Enhance Threat Detection</h3>
           <p>A SIEM (Security Information and Event Management) tool is a comprehensive cybersecurity solution that...</p>
           <a href='#'>Read more</a>
         </div>
-        <div className="card">
+        <div className="card-blog">
           <span className="date">18 April 2024</span>
           <h3>Demystifying MITRE ATT&CK: A Beginner’s Guide</h3>
           <p>MITRE ATT&CK (Adversarial Tactics, Techniques, and Common Knowledge) is a framework....</p>
           <a href='#'>Read more</a>
         </div>
-        <div className="card">
+        <div className="card-blog">
           <span className="date">18 April 2024</span>
           <h3>EncroChat: The Dark Side of Secure Communication</h3>
           <p>In the murky underworld of organised crime, communication is a double-edged sword. Criminals need to co...</p>
