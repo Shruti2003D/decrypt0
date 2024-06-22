09945bd157c5bfe458cd354067a9dea2bb39f4ce
Om Jangde <om.jangade@mitaoe.ac.in>
2024-06-23 00:19:00 +0530

    Dashboard_Basic-UI

--- menterbot/src/Components/dashboard.jsx
+++ menterbot/src/Components/dashboard.jsx
@@ -1,15 +1,40 @@
 import React from 'react'
+// import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { FaRegCircleUser } from "react-icons/fa6";
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+// import ROLE from '../common/role';
+import '../css/dashboard.css'; // Import the CSS file
+import logo from "../assets/logo.png"
 
 const Dashboard = () => {
   return (
-    <div>
-      This is dashboard
-      This is dashboard
-      This is dashboard
-      This is dashboard
-      This is dashboard
-      This is dashboard
+
+    <div className='dashboard-container'>
+      <aside className='dashboard-aside'>
+        <img src={logo} alt="Logo" className='dashboard-logo' />
+        <nav className='dashboard-nav'>
+          <Link to={""} className='nav-link'>Dashboard</Link>
+        </nav>
+      </aside>
+
+      <main className='dashboard-main'>
+        <h1>Dashboard</h1>
+        <h2>Welcome back, OM</h2>
+        <div className=''></div>
+
+        <div className='dashboard-body'>
+          <h3>Meeting Request</h3>
+          <div className=''>
+            <a href="https://mentorbot.in/students/meeting-requests/create" class="button orange-btn">Add Meeting Request</a>
+          </div>
+          <h3>Recommended Courses</h3>
+          <h3>Your Courses</h3>
+        </div>
+      </main>
+
     </div>
+
   )
 }
 
